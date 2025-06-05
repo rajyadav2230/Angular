2c4a24b4aca54fb22f99708b29d3d7189d05a303
rajyadav2230 <yadavraj2230@gmail.com>
2025-06-05 11:01:37 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-echo "Starting backend app..."
+echo "Running ApplicationStart Script"
 
-cd /home/ubuntu/backend-app
+cd /home/ec2-user/backend-app
+
+# Start the app in the background
 nohup java -jar backend.jar > backend.log 2>&1 &
-
+echo "Application started with PID $!"
