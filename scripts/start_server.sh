bd5ba04e3157f4be361c491f6cf905ec5924843e
rajyadav2230 <yadavraj2230@gmail.com>
2025-06-05 11:22:40 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Running ApplicationStart Script"
 
-cd /home/ec2-user/backend-app
+cd /home/ubuntu/backend-app
 
-# Start the app in the background
+# Start Spring Boot app in background
 nohup java -jar backend.jar > backend.log 2>&1 &
-echo "Application started with PID $!"
+echo "App started successfully"
